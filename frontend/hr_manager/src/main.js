757fe385e49e373f9f0ebe9f360f20554d59805c
Caio Andrade <caio_93@live.com>
2021-10-23 20:23:11 +1100

    a
    aa

--- frontend/hr_manager/src/main.js
+++ frontend/hr_manager/src/main.js
@@ -6,7 +6,6 @@ import {routes} from './routes.js';
 
 import axios from 'axios';
 axios.defaults.timeout = 5000;
-//axios.defaults.baseURL = 'http://localhost:3000';
 
 Vue.config.productionTip = false
 const router = new VueRouter({
